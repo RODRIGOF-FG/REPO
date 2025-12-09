81de8c134b93eea00009eee9a5304b090bc16adf
RODRIGOF-FG <ffloriang@autonoma.edu.pe>
2025-12-09 01:30:35 -0500

    agreagdo final

--- sistema_web/src/main/java/com/example/sistema_procesos/registrar_salida/SalidaService.java
+++ sistema_web/src/main/java/com/example/sistema_procesos/registrar_salida/SalidaService.java
@@ -8,5 +8,23 @@ import java.time.LocalDate;
 @Service
 @RequiredArgsConstructor
 public class SalidaService {
-    
+      private final SalidaRepository repository;
+
+    public Salida registrarSalida(User user) {
+
+        LocalDate hoy = LocalDate.now();
+
+        if (repository.findByUserIdAndFechaSalida(user.getId(), hoy).isPresent()) {
+            throw new RuntimeException("Ya registraste tu salida hoy");
+        }
+
+        Salida salida = Salida.builder()
+                .userId(user.getId())
+                .nombreUsuario(user.getFirstname() + " " + user.getLastname())
+                .role(user.getRole().name())
+                .fechaSalida(hoy)
+                .build();
+
+        return repository.save(salida);
+    }
 }
