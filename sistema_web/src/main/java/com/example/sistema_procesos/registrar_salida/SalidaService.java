6f39ce1221820753b685912a06521f99ccbd9b70
RODRIGOF-FG <ffloriang@autonoma.edu.pe>
2025-12-09 01:29:39 -0500

    agregado de liberias

--- sistema_web/src/main/java/com/example/sistema_procesos/registrar_salida/SalidaService.java
+++ sistema_web/src/main/java/com/example/sistema_procesos/registrar_salida/SalidaService.java
@@ -1,5 +1,12 @@
 package com.example.sistema_procesos.registrar_salida;
+import com.example.sistema_procesos.User.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+
+@Service
+@RequiredArgsConstructor
 public class SalidaService {
     
 }
