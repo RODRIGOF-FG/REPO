f10e37f3253729f8e9e0d87718d2e91d19cf9b00
RODRIGOF-FG <ffloriang@autonoma.edu.pe>
2025-12-08 20:04:13 -0500

    Subo carpeta con varios archivos

---
+++ sistema_procesos/src/main/java/com/example/sistema_procesos/Operador/OperadorController.java
@@ -0,0 +1,24 @@
+package com.example.sistema_procesos.Operador;
+
+import com.example.sistema_procesos.User.User;
+import com.example.sistema_procesos.User.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/operador")
+@RequiredArgsConstructor
+public class OperadorController {
+
+    private final UserRepository userRepository;
+
+    @GetMapping("/profile")
+    public ResponseEntity<?> profile(Authentication auth) {
+        String username = auth.getName();
+        User user = userRepository.findByEmail(username).orElse(null);
+        if (user == null) return ResponseEntity.status(404).body("Usuario no encontrado");
+        return ResponseEntity.ok(user);
+    }
+}
