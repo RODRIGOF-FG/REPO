58b404eaa36b5fe431d523361f65df530d2a3feb
Yoyialx <yordialvaradovasquez@gmail.com>
2025-12-09 15:21:35 -0500

    comentario

--- sistema_web_p/src/main/java/com/example/sistema_procesos/Asistencia/AsistenciaController.java
+++ sistema_web_p/src/main/java/com/example/sistema_procesos/Asistencia/AsistenciaController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 public class AsistenciaController {
 
     private final AsistenciaService service;
+    // hola
 
     // ✅ REGISTRAR ASISTENCIA
     @PostMapping
